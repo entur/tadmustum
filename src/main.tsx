4eb31b782be8ba83896da573e2069db1036ecd91
Arnljot Arntsen <arnljot.arntsen@entur.org>
2025-05-06 13:35:49 +0200

    Added skelleton for Auth and Config.
     Conflicts:
    	package-lock.json
    	package.json
    	src/App.tsx
    	src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import {fetchConfig} from "./config/fetchConfig.tsx";
+import { ConfigContext } from './config/ConfigContext.tsx';
+import { AuthProvider } from './auth/Auth.tsx';
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+fetchConfig().then((config) => {
+    createRoot(document.getElementById('root')!).render(
+        <StrictMode>
+            <ConfigContext.Provider value={config}>
+                <AuthProvider>
+                    <App />
+                </AuthProvider>
+            </ConfigContext.Provider>
+        </StrictMode>,
+    )
+})
