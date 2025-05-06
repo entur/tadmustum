a6e093cb8b8ac8b141faf218cf3f9a8baa97b723
Arnljot Arntsen <arnljot.arntsen@entur.org>
2025-05-06 09:22:23 +0200

    Justeringer.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import './App.css'
 import {Box} from "@mui/material";
 import Header from "./components/Header.tsx";
+import {SearchProvider} from "./components/SearchContext.tsx";
 
 function App() {
 
 
   return (
       <Box sx={{ flexGrow: 1 }}>
+          <SearchProvider>
           <Header/>
-
+          </SearchProvider>
       </Box>
   )
 }
