01e9fd8a7f7d7e30047d9d1fae2810ed49aed437
Arnljot Arntsen <arnljot.arntsen@entur.org>
2025-05-06 14:47:02 +0200

    Changed port and client id. Change clientid to your own when developing new apps.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT ? Number(process.env.PORT) : 9000,
+    port: process.env.PORT ? Number(process.env.PORT) : 5000,
   },
 });
