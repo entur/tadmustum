bf8aeb2a67ffa4593bec3b693a27bf6e8819122b
Arnljot Arntsen <arnljot.arntsen@entur.org>
2025-06-16 11:51:14 +0200

    Added links to features on landing page.

--- src/features/landing-page/Home.tsx
+++ src/features/landing-page/Home.tsx
@@ -1,20 +1,22 @@
-import { Container, Box, Typography } from "@mui/material";
+import { Container, Box, Typography, Link } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
     <Container maxWidth="md">
       <Box sx={{ my: 8, textAlign: "left" }}>
         <Typography variant="h3" component="h1" gutterBottom>
           Welcome to EnTur Car Pooling PoC
         </Typography>
-        <Typography variant="subtitle1">
+        <Typography variant="body1" sx={{ mb: 2 }}>
           Enturs Car Pooling PoC frontend app for testing creation of Car
           Pooling messages for making such trips available in a journey planner.
           This app uses a GraphQL api to create trips in a backend app, this
           backend app will then relay these to the journey planner in SIRI
           format.
         </Typography>
-        <br />
         <Typography variant="body1">
           EnTur Car Pooling PoC cannot at the moment describe:
         </Typography>
@@ -33,15 +35,21 @@ export default function Home() {
             Number of free seats in the car
           </Typography>
         </Box>
-        <Typography variant="body1">
+        <Typography variant="body1" sx={{ mb: 2 }}>
           The current state of the PoC is to enable non technical users to
           create SIRI messages that can be consumed by a journey planner, and
           verify that the journey planner can create flexible temporary stop
           places for Car Pooling trips.
         </Typography>
-        <Typography variant="body1">
+        <Typography variant="body1" sx={{ mb: 2 }}>
           Next step is to see if SIRI is a good fit for Car Pooling messages and
           what needs to be put into a profile to facilitate it's adoption.
+        </Typography>
+        <Typography variant="body1">
+          Go to <Link onClick={() => navigate("/plan-trip")}>Plan trip</Link> to
+          create a new Car Pooling Message, or{" "}
+          <Link onClick={() => navigate("/trips")}>Planned trips</Link> to see
+          messages that's created.
         </Typography>
       </Box>
     </Container>
